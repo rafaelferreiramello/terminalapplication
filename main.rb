39bffade32687e2ee875e5b330c31f2d59a4eaa7
Rafael Mello <rafael.ferreira.sobral@gmail.com>
2021-04-04 20:49:43 +1000

    Signup code

--- main.rb
+++ main.rb
@@ -11,3 +11,35 @@
 # If username is not part of CVC file, login.csv, "You don't have a account, sign up or try again" take back to user input
 # Clear old commands before presenting "Menu"
 # Clear old commands before presenting "Menu"
+
+# quit = false 
+# while !quit
+#     input = gets.chomp
+#     if input == "quit"
+#         quit = true
+#     end
+# end 
+users_array = []
+
+until quit
+    puts "options: [login, signup]"
+    # signup 
+    input = gets.chomp
+    if input == "signup"
+        puts "what is your username?"
+        username = gets.chomp
+        puts "what is your password?"
+        password = gets.chomp 
+        user = {}
+        user[:username] = username
+        user[:password] = password 
+        users_array.push(user)
+        File.open("users.csv", "a") {
+            |file| file.append("#{username}, #{password}\n")
+        } 
+    elsif input == "login"
+    end 
+    # login
+    puts "what would you like to do?"
+    puts "options: quit"
+    input = gets.chomp 
