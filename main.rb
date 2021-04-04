b5ca8340cd4a1323c36a06c1a11f416c3d68b90b
Rafael Mello <rafael.ferreira.sobral@gmail.com>
2021-04-04 16:48:36 +1000

    Login pseudocode

--- main.rb
+++ main.rb
@@ -1,1 +1,13 @@
 # TERMINAL APPLICATION - HOW YOU DOIN?
+
+# login
+# Open external CSV file, login.csv
+# Give user the login option
+# Get user input. Username and Password
+# Check if Username exits on the CSV file, login.csv
+# If username is part of CVC file, login.csv, check if Password match with Username value
+# If password match Username value, "Welcome" and present "Menu"
+# If password does not match Username value, "Password Incorrect, try again", take back to user input
+# If username is not part of CVC file, login.csv, "You don't have a account, sign up or try again" take back to user input
+# Clear old commands before presenting "Menu"
+# Clear old commands before presenting "Menu"
